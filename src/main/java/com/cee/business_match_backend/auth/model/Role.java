e6e610839bc2c2bb226f765feb3e6c73f2061c6a
peterblasko <blasko.peter.hu@gmail.com>
2026-03-29 11:20:07 +0200

    Add user entity role enum and repository

---
+++ src/main/java/com/cee/business_match_backend/auth/model/Role.java
@@ -0,0 +1,9 @@
+package com.cee.business_match_backend.auth.model;
+
+public enum Role {
+
+    INVESTOR,
+    EXPORTER,
+    LAW_FIRM
+
+}
---
+++ src/main/java/com/cee/business_match_backend/common/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.cee.business_match_backend.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Backend is running";
+    }
+
+}
